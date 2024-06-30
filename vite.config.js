56974172dd5fb1e726b21acab5b26846defccf10
0xduer <0xduer@gmail.com>
2024-06-30 22:16:38 +0800

    feat:配置路径别名

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    server: {
+        port: 3000,
+        open: true,
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@v': path.resolve(__dirname, './src/view'),
+        },
+    },
 })
