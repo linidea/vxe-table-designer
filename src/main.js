e2e25ceef85d07365cae5783c41d5a068dddc2ba
0xduer <0xduer@gmail.com>
2024-07-05 01:10:09 +0800

    feat:全局配置 element-plus

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import './style.css'
-import {createApp} from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+//
+import './style.css';
+import 'element-plus/dist/index.css';
+//
+import {createApp} from 'vue';
+import ElementPlus from 'element-plus';
+//
+import App from './App.vue';
+//
+const app = createApp(App);
+app.use(ElementPlus);
+app.mount('#app')
