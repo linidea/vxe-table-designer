a8c19c235bd84cb5eb907ff09f4b29dda3c28548
0xduer <0xduer@gmail.com>
2024-07-08 00:37:55 +0800

    feat:新增 vxe-table 依赖于配置

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 //
+import {createApp} from 'vue';
+import App from './App.vue';
 import './style.css';
+//
+import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 //
-import {createApp} from 'vue';
-import ElementPlus from 'element-plus';
-//
-import App from './App.vue';
+import VxeTable from "vxe-table";
+import "vxe-table/lib/style.css";
 //
 const app = createApp(App);
 app.use(ElementPlus);
+app.use(VxeTable);
 app.mount('#app')
