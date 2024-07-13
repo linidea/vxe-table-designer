82f1297e07948fcb61dccb54f13be1c80bc081a4
0xduer <0xduer@gmail.com>
2024-07-14 03:48:22 +0800

    feat:全局注册 lodash

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import {createApp} from 'vue';
 import App from './App.vue';
 import './style.css';
 //
-import {Pinia} from './store';
+import _ from 'lodash';
+import {Pinia} from '@store';
 //
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
@@ -12,6 +13,7 @@ import VxeTable from "vxe-table";
 import "vxe-table/lib/style.css";
 //
 const app = createApp(App);
+app.config.globalProperties.lodash = _;
 app.use(Pinia);
 app.use(ElementPlus);
 app.use(VxeTable);
