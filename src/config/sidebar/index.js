d456a210d0a9af6b6cdfae73a8913b153e48f8f4
0xduer <0xduer@gmail.com>
2024-07-14 05:19:38 +0800

    feat:新增侧边栏json 配置

---
+++ src/config/sidebar/index.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        name: "tableBase",
+        icon: "grid",
+        intro: "表格基础",
+    },
+    {
+        name: "tableColumn",
+        icon: "column",
+        intro: "表格列项",
+    },
+    {
+        name: "tableOption",
+        icon: "json",
+        intro: "表格配置",
+    },
+    {
+        name: "tableData",
+        icon: "database",
+        intro: "表格数据"
+    }
+]
