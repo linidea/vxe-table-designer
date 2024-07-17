aa55dc2c2386c63454d36c31bae10bc2234d60ba
0xduer <0xduer@gmail.com>
2024-07-17 22:52:38 +0800

    feat:优化 vite 配置

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+    ],
     server: {
         port: 8000,
         open: true,
