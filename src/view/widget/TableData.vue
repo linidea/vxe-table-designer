917aa95c50f546574d52fa7f6c9a48bcdb7d359e
0xduer <0xduer@gmail.com>
2024-07-17 23:06:17 +0800

    feat:新增表格数据编辑区域

--- src/view/widget/TableData.vue
+++ src/view/widget/TableData.vue
@@ -1,11 +1,25 @@
-<script>
-
-</script>
-
 <template>
-
+  <MonacoEditor language="json" :value="tableData" @update:value="updateTableData"/>
 </template>
+<script setup>
+import MonacoEditor from '@components/monaco/index.vue';
+//
+import {computed} from 'vue'
+//
+import {useTableStore} from "@store/modules/table.js";
 
+const tableStore = useTableStore();
+//
+const tableData = computed(() => JSON.stringify(tableStore.tableData, null, 2));
+//
+const updateTableData = (value) => {
+  try {
+    tableStore.setTableData(value);
+  } catch (error) {
+    console.error('Invalid JSON:', error);
+  }
+}
+</script>
 <style scoped>
 
 </style>
