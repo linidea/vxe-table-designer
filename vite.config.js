83806068f3645a6818cd405c838a2a1770894610
0xduer <0xduer@gmail.com>
2024-07-25 01:37:38 +0800

    feat:新增 style 路径别名

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
+            '@style': path.resolve(__dirname, './src/style'),
             '@store': path.resolve(__dirname, './src/store'),
             '@view': path.resolve(__dirname, './src/view'),
             '@config': path.resolve(__dirname, './src/config'),
