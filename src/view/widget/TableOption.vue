9ad95ca1c6004e75781fd3e4e66446280a880c05
0xduer <0xduer@gmail.com>
2024-07-25 01:38:08 +0800

    feat:新增表单配置模块

---
+++ src/view/widget/TableOption.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="widget-title">表格配置</div>
+  <MonacoEditor language="json" :value="''" @update:value="updateOption"/>
+</template>
+<script setup>
+import MonacoEditor from '@components/monaco/index.vue';
+
+//
+const updateOption = (value) => {
+  console.log(value);
+}
+</script>
+<style scoped>
+
+</style>
